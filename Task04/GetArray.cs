c8581bd9ff7ca540da50fa3a9bd1b11ce60ca36c
Igor Korzhan <igor.human@gmail.com>
2024-07-20 22:06:40 +0300

    Task 04

---
+++ Task04/GetArray.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyListExample
+{
+    public static class MyExtensions
+    {
+        public static T[] GetArray<T>(this IEnumerable<T> list)
+        {
+            List<T> tempList = new List<T>();
+            foreach (var item in list)
+            {
+                tempList.Add(item);
+            }
+            return tempList.ToArray();
+        }
+    }
+}
+
---
+++ Task04/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyListExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> myList = new MyList<int>();
+            myList.Add(1);
+            myList.Add(2);
+            myList.Add(3);
+            myList.Add(4);
+            myList.Add(5);
+
+            int[] array = myList.GetArray();
+
+            Console.WriteLine("Elements in the array returned by GetArray:");
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
